a9466699a0f8cfaae513938552dbfd7f39e4df92
Tomas Orti <tomas.orti.martin@ericsson.com>
2017-12-06 03:15:11 +0100

    Split functions in main file and delete #include file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,47 @@
-#include "pacman.h"
+#include <iostream>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#include "cmdline_options.h"
+#include "constants.h"
+#include "game.h"
+#include "ghost_figur.h"
+#include "pacman_figur.h"
+#include "menu_main.h"
+#include "highscore.h"
+
 #define VERSION 1.0
+
+void getCommandLine(int argc, char *argv[]);
+void cleanUp();
+void highscore();
 
 int main(int argc, char *argv[]) {
 
     srand((unsigned int)time(0)); // init randomize
+
+    getCommandLine(argc, argv);
+    
+    if (Screen::getInstance()->hasSDLInitErrorOccured())
+        return EXIT_FAILURE;
+
+    while (MenuMain::getInstance()->show()) {
+
+        Game* game = Game::getInstance();
+        game->start();
+        if (game->isGameOver()) {
+            // do not load() here, this has been done at the time the game was over
+            highscore();
+        }
+    }
+
+    cleanUp();
+    return EXIT_SUCCESS;
+}
+
+void getCommandLine(int argc, char *argv[]) {
 
     CommandLineOptions::set(argc, argv);
     if (CommandLineOptions::exists("h", "help")) {
@@ -22,33 +60,32 @@ int main(int argc, char *argv[]) {
                   << "  --highscore=...    Define path to highscore file."                   << std::endl
                   << "  --hs-key=...       Highscore encryption key (hexadecimal)."          << std::endl
                   << std::endl;
-        return EXIT_SUCCESS;
+        exit(EXIT_SUCCESS);
     }
     else if (CommandLineOptions::exists("v", "version")) {
         std::cout << "pacman version " << VERSION << std::endl;
-        return EXIT_SUCCESS;
-    }
+        exit(EXIT_SUCCESS);
+    }   
+}
 
-    if(Screen::getInstance()->hasSDLInitErrorOccured())
-        return EXIT_FAILURE;
+void highscore() {
 
-    while(MenuMain::getInstance()->show()) {
-        Game::getInstance()->start();
-        if (Game::getInstance()->isGameOver()) {
-            // do not load() here, this has been done at the time the game was over
-            if (!HighscoreList::getInstance()->isReadonly()) {
-                HighscoreList::getInstance()->show(true, true);  // player name alterable, last entry highlighted
-                if (HighscoreList::getInstance()->getIdxLastInsertedEntry() >= 0) {
-                    HighscoreEntry *entry = HighscoreList::getInstance()->getEntry(HighscoreList::getInstance()->getIdxLastInsertedEntry());
-                    entry = new HighscoreEntry(std::string(entry->getPlayerName()), entry->getScore(), entry->getLevel());
-                    HighscoreList::getInstance()->load();
-                    HighscoreList::getInstance()->insertEntry(entry);
-                    HighscoreList::getInstance()->save();
-                }
-                HighscoreList::getInstance()->show(false, true);
-            }
+    HighscoreList* highscoreList = HighscoreList::getInstance();
+    if (!highscoreList->isReadonly()) {
+
+        highscoreList->show(true, true);  // player name alterable, last entry highlighted
+        if (highscoreList->getIdxLastInsertedEntry() >= 0) {
+            HighscoreEntry *entry = highscoreList->getEntry(highscoreList->getIdxLastInsertedEntry());
+            entry = new HighscoreEntry(std::string(entry->getPlayerName()), entry->getScore(), entry->getLevel());
+            highscoreList->load();
+            highscoreList->insertEntry(entry);
+            highscoreList->save();
         }
-    }
+        highscoreList->show(false, true);
+    }    
+}
+
+void cleanUp() {
 
     Game::cleanUpInstance();
     MenuMain::cleanUpInstance();
@@ -58,7 +95,5 @@ int main(int argc, char *argv[]) {
     Sounds::cleanUpInstance();
     Screen::cleanUpInstance();
     HighscoreList::cleanUpInstance();
-    CommandLineOptions::cleanUp();
-
-    return EXIT_SUCCESS;
+    CommandLineOptions::cleanUp();    
 }
